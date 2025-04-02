97432c067a20ebc7cefd0be86d72e9c65d6ac128
yas3in <jastinafa2016@gmail.com>
2025-04-02 13:26:18 +0330

    feat: create signup view

--- apps/account/urls/front.py
+++ apps/account/urls/front.py
@@ -1,9 +1,9 @@
 from django.urls import path
 
-from apps.account.views.front import FrontLoginView, FrontSigninView
+from apps.account.views.front import signup_view, login_view
 
 
 urlpatterns = [
-    path("login/", FrontLoginView.as_view(), name="login"),
-    path("signin/", FrontSigninView.as_view(), name="signin"),
+    path("login/", login_view, name="login"),
+    path("signup/", signup_view, name="signup"),
 ]
--- apps/account/views/front.py
+++ apps/account/views/front.py
@@ -1,8 +1,10 @@
+from django.http import Http404, HttpResponseRedirect
 from django.shortcuts import redirect, render
 
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.views import LoginView
+from django.contrib.auth.models import User
 
 
 class FrontLoginView(LoginView):
@@ -15,5 +17,42 @@ class FrontLoginView(LoginView):
         return super().dispatch(request, *args, **kwargs)
     
 
-class FrontSigninView(LoginView):
-    pass
+def login_view(request):
+    if request.method == "POST":
+        response = request.POST.get('next', '/')
+
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            if response.endswith('login/'):
+                return redirect('user-panel')
+            return HttpResponseRedirect(response)
+        return redirect('login')
+    else:
+        if request.user.is_authenticated:
+            return redirect('user-panel')
+    return render(request, "account/login.html")
+
+
+def signup_view(request):
+    if request.method == "POST":
+        response = request.POST.get('next', '/')
+        username = request.POST.get("username")
+        email = request.POST.get("email")
+        password = request.POST.get("password")
+        if username and email and password is not None:
+            sign_up = User.objects.create_user(username=username, email=email, password=password)
+            if sign_up:
+                user = authenticate(request, username=username, password=password)
+                login(request, user)
+                if response.endswith('signup/'):
+                    return redirect('user-panel')
+                return HttpResponseRedirect(response)
+            return redirect('login')
+        return redirect('signup')
+    else:
+        return render(request, "account/signup.html")
+
