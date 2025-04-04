a78703800af6d45c7f5cd3685fb8ce30397138fc
yas3in <jastinafa2016@gmail.com>
2025-04-04 18:12:17 +0330

    fix: fix model report

--- apps/report/admin.py
+++ apps/report/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from django.contrib.admin import register
+from .models import Answer, Question, Report
 
-# Register your models here.
+
+@register(Answer)
+class AnswerAdmin(admin.ModelAdmin):
+    list_display = ("answer",)
+
+
+@register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ("question",)
+
+
+class AnswerInline(admin.TabularInline):
+    model = Answer
+
+
+class QuestionInline(admin.TabularInline):
+    model = Question
+
+
+@register(Report)
+class ReportAdmin(admin.ModelAdmin):
+    list_display = ("name", "user", "answer", "question")
+    inlines = [QuestionInline, AnswerInline]
--- apps/report/models.py
+++ apps/report/models.py
@@ -2,16 +2,19 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
+class Report(models.Model):
+    name = models.CharField()
+    description = models.TextField()
+    user = models.ForeignKey(User, related_name="user", null=True, blank=True, on_delete=models.CASCADE)
+    created_time = models.TimeField(auto_now=True)
+
+
 class Question(models.Model):
+    name = models.ForeignKey(Report, related_name="question", on_delete=models.CASCADE)
     question = models.TextField()
 
 
 class Answer(models.Model):
+    name = models.ForeignKey(Report, related_name="answer", on_delete=models.CASCADE)
     answer = models.TextField()
 
-
-class Report(models.Model):
-    name = models.CharField()
-    user = models.ForeignKey(User, related_name="user", null=True, blank=True, on_delete=models.CASCADE)
-    question = models.ForeignKey(Question, related_name="user", on_delete=models.CASCADE)
-    answer = models.ForeignKey(Answer, related_name="asnwer", null=True, blank=True, on_delete=models.CASCADE)
