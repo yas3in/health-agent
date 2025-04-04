c9d82419338298cd968ee2529c83c0b72653d62a
yas3in <jastinafa2016@gmail.com>
2025-04-04 17:58:39 +0330

    feat: create model Report and Answer and Question

--- apps/report/models.py
+++ apps/report/models.py
@@ -12,6 +12,6 @@ class Answer(models.Model):
 
 class Report(models.Model):
     name = models.CharField()
-    user = models.ForeignKey(User, related_name="user", null=True, blank=True)
-    question = models.ForeignKey(Question, related_name="user")
-    answer = models.ForeignKey(Answer, related_name="asnwer", null=True, blank=True)
+    user = models.ForeignKey(User, related_name="user", null=True, blank=True, on_delete=models.CASCADE)
+    question = models.ForeignKey(Question, related_name="user", on_delete=models.CASCADE)
+    answer = models.ForeignKey(Answer, related_name="asnwer", null=True, blank=True, on_delete=models.CASCADE)
