405796638cec8246d53222559bc219d251fe1bbc
yas3in <jastinafa2016@gmail.com>
2025-07-31 22:35:23 +0330

    Feat: add button next and change wuestion

--- apps/report/static/report/js/report_detail.js
+++ apps/report/static/report/js/report_detail.js
@@ -6,6 +6,20 @@ questions.forEach( (value, index) => {
     question_object[index] = value.textContent;
 });
 
-console.log(question_object);
 
+let next = document.querySelector("#next")
 
+let counter = 0
+let question = document.querySelector("#question")
+question.innerHTML = question_object[counter]
+next.addEventListener("click", ()=> {
+    counter += 1;
+    if (question_object[counter]) {
+        question.innerHTML = question_object[counter]
+        
+    } else {
+        next.disabled = true
+        console.log("finish");
+        
+    }
+})
